bd1cb143ae075ba63a9aa47fc886aee873c8a257
norbertc5 <113235406+norbertc5@users.noreply.github.com>
2023-04-20 16:24:23 +0200

    20.04.23
    Added borders, trigger

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -32,18 +32,30 @@
 		private void InitializeComponent()
 		{
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Screen));
             this.SnakeHead = new System.Windows.Forms.PictureBox();
             this.MainTimer = new System.Windows.Forms.Timer(this.components);
             this.Apple = new System.Windows.Forms.PictureBox();
+            this.Border1 = new System.Windows.Forms.PictureBox();
+            this.Border2 = new System.Windows.Forms.PictureBox();
+            this.Border3 = new System.Windows.Forms.PictureBox();
+            this.Border4 = new System.Windows.Forms.PictureBox();
+            this.Trigger = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.SnakeHead)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Apple)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Border1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Border2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Border3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Border4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Trigger)).BeginInit();
             this.SuspendLayout();
             // 
             // SnakeHead
             // 
+            this.SnakeHead.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.SnakeHead.BackColor = System.Drawing.Color.Blue;
-            this.SnakeHead.Location = new System.Drawing.Point(10, 10);
+            this.SnakeHead.Location = new System.Drawing.Point(74, 131);
             this.SnakeHead.Name = "SnakeHead";
             this.SnakeHead.Size = new System.Drawing.Size(20, 20);
             this.SnakeHead.TabIndex = 0;
@@ -56,13 +68,62 @@
             // 
             // Apple
             // 
-            this.Apple.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.Apple.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Apple.BackgroundImage")));
-            this.Apple.Location = new System.Drawing.Point(200, 10);
+            this.Apple.BackColor = System.Drawing.Color.Red;
+            this.Apple.Location = new System.Drawing.Point(218, 131);
             this.Apple.Name = "Apple";
             this.Apple.Size = new System.Drawing.Size(20, 20);
             this.Apple.TabIndex = 1;
             this.Apple.TabStop = false;
+            // 
+            // Border1
+            // 
+            this.Border1.BackColor = System.Drawing.Color.Green;
+            this.Border1.Location = new System.Drawing.Point(20, 60);
+            this.Border1.Name = "Border1";
+            this.Border1.Size = new System.Drawing.Size(20, 460);
+            this.Border1.TabIndex = 2;
+            this.Border1.TabStop = false;
+            this.Border1.Tag = "GameOverWhenTouch";
+            // 
+            // Border2
+            // 
+            this.Border2.BackColor = System.Drawing.Color.Green;
+            this.Border2.Location = new System.Drawing.Point(740, 60);
+            this.Border2.Name = "Border2";
+            this.Border2.Size = new System.Drawing.Size(20, 460);
+            this.Border2.TabIndex = 3;
+            this.Border2.TabStop = false;
+            this.Border2.Tag = "GameOverWhenTouch";
+            // 
+            // Border3
+            // 
+            this.Border3.BackColor = System.Drawing.Color.Green;
+            this.Border3.Location = new System.Drawing.Point(20, 60);
+            this.Border3.Name = "Border3";
+            this.Border3.Size = new System.Drawing.Size(740, 20);
+            this.Border3.TabIndex = 4;
+            this.Border3.TabStop = false;
+            this.Border3.Tag = "GameOverWhenTouch";
+            // 
+            // Border4
+            // 
+            this.Border4.BackColor = System.Drawing.Color.Green;
+            this.Border4.Location = new System.Drawing.Point(20, 500);
+            this.Border4.Name = "Border4";
+            this.Border4.Size = new System.Drawing.Size(740, 20);
+            this.Border4.TabIndex = 5;
+            this.Border4.TabStop = false;
+            this.Border4.Tag = "GameOverWhenTouch";
+            // 
+            // Trigger
+            // 
+            this.Trigger.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Trigger.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.Trigger.Location = new System.Drawing.Point(115, 131);
+            this.Trigger.Name = "Trigger";
+            this.Trigger.Size = new System.Drawing.Size(20, 20);
+            this.Trigger.TabIndex = 6;
+            this.Trigger.TabStop = false;
             // 
             // Screen
             // 
@@ -70,6 +131,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGreen;
             this.ClientSize = new System.Drawing.Size(781, 561);
+            this.Controls.Add(this.Trigger);
+            this.Controls.Add(this.Border4);
+            this.Controls.Add(this.Border3);
+            this.Controls.Add(this.Border2);
+            this.Controls.Add(this.Border1);
             this.Controls.Add(this.Apple);
             this.Controls.Add(this.SnakeHead);
             this.Name = "Screen";
@@ -77,8 +143,19 @@
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainFormKeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.SnakeHead)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Apple)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Border1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Border2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Border3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Border4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Trigger)).EndInit();
             this.ResumeLayout(false);
 
 		}
+
+        private System.Windows.Forms.PictureBox Border1;
+        private System.Windows.Forms.PictureBox Border2;
+        private System.Windows.Forms.PictureBox Border3;
+        private System.Windows.Forms.PictureBox Border4;
+        private System.Windows.Forms.PictureBox Trigger;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -7,15 +7,18 @@ namespace snake
 {
 	public partial class Screen : Form
 	{
+		// enums
 		enum Directions {Up, Down, Left, Right};
-		enum WidthOrHeight {Width, Height};
-		
-		Directions actualDirection = Directions.Right;
-		
-		int speed = 20;
-		int a;
-		Random random;
+		enum WidthOrHeight {Width, Height};  // using to "draw" background pasks
+
+		// lists
 		List<PictureBox> snakeBodyParts = new List<PictureBox>();
+
+		// variables
+		Directions actualDirection = Directions.Right;		
+		int stepLenght = 20;  // how big step make snake in one tick
+		Random random;
+		bool canSnakeMove = true;
 		
 		public Screen()
 		{
@@ -23,54 +26,95 @@ namespace snake
 			CreateBackgroundPasks(WidthOrHeight.Width);
 			CreateBackgroundPasks(WidthOrHeight.Height);			
 			
-			SnakeHead.Location = new Point(0, 0);
+			// set start locations
+			SnakeHead.Location = new Point(100, 100);
+			Apple.Location = new Point(240, 100);
+
 			random = new Random();
 			snakeBodyParts.Add(SnakeHead);
+			Trigger.Parent = SnakeHead;
+
+			// set start snake lenght
 			CreateBody();
-        }
+			CreateBody();
+
+			//this.Controls.SetChildIndex(Trigger, 1);
+		}
 		
 		void MainTimerTick(object sender, EventArgs e)
-		{		
-            #region Moving body
-            for (int i = snakeBodyParts.Count - 1; i > 0; i--)
+		{
+			if(canSnakeMove)
             {
-            	// all snake body parts are moving to next body part location in each tick
-                snakeBodyParts[i].Location = snakeBodyParts[i - 1].Location;
-            }
-            #endregion
+				#region Moving body
 
-            #region Movement
-            // move snakeHead
-            Point shl = SnakeHead.Location;
-			switch(actualDirection)
-			{
-					case Directions.Left: shl = new Point(shl.X - speed, shl.Y);
-						break;
-						
-					case Directions.Right: shl = new Point(shl.X + speed, shl.Y);
+				for (int i = snakeBodyParts.Count - 1; i > 0; i--)
+				{
+					// all snake body parts are moving to next body part location in each tick
+					snakeBodyParts[i].Location = snakeBodyParts[i - 1].Location;
+				}
+
+				#endregion
+
+				#region Move snake head
+
+				// move snake head according to actualDirection
+				Point snakeHeadLoc = SnakeHead.Location;
+				switch (actualDirection)
+				{
+					case Directions.Left:
+						snakeHeadLoc = new Point(snakeHeadLoc.X - stepLenght, snakeHeadLoc.Y);
 						break;
 
-					case Directions.Up: shl = new Point(shl.X, shl.Y - speed);
-						break;						
-						
-					case Directions.Down: shl = new Point(shl.X, shl.Y + speed);
-						break;							
+					case Directions.Right:
+						snakeHeadLoc = new Point(snakeHeadLoc.X + stepLenght, snakeHeadLoc.Y);
+						break;
+
+					case Directions.Up:
+						snakeHeadLoc = new Point(snakeHeadLoc.X, snakeHeadLoc.Y - stepLenght);
+						break;
+
+					case Directions.Down:
+						snakeHeadLoc = new Point(snakeHeadLoc.X, snakeHeadLoc.Y + stepLenght);
+						break;
+				}
+
+				SnakeHead.Location = snakeHeadLoc;
+				Trigger.Left = (Trigger.Parent.Width - Trigger.Width) / 2;
+				Trigger.Top = (Trigger.Parent.Height - Trigger.Height) / 2;
+				//Trigger.Location = new Point(SnakeHead.Width / 2 - this.Width / 2, SnakeHead.Height / 2 - this.Height / 2);
+
+				#endregion
 			}
-			
-			SnakeHead.Location = shl;			
-			#endregion
-			
-			#region When eat apple		
+
+			#region When eat apple
+
 			if (SnakeHead.Bounds.IntersectsWith(Apple.Bounds))
 			{
-                int x = random.Next(10, Size.Width - 10);
-				x = (int)Math.Round(Convert.ToDecimal((x + 0.0000001) / 10.00)) * 10; // snapping to grid
-                int y = random.Next(10, Size.Height - 10);
-				y = (int)Math.Round(Convert.ToDecimal((y + 0.0000001) / 10.00)) * 10;
+				// draw lots number so that apple set in playing area
+				int x = random.Next(60, Size.Width - 60);
+				int y = random.Next(80, Size.Height - 60);
+
+				// round number and snap apple to grid
+				x = (int)Math.Round(Convert.ToDecimal((x + 0.0000001) / 20.00)) * 20;
+				y = (int)Math.Round(Convert.ToDecimal((y + 0.0000001) / 20.00)) * 20;
+
                 Apple.Location = new Point(x, y);
                 CreateBody();
             }
+
             #endregion
+
+			foreach(Control obj in this.Controls)
+            {
+				if(obj is PictureBox && (string)obj.Tag == "GameOverWhenTouch")
+                {
+					if(Trigger.Bounds.IntersectsWith(obj.Bounds))
+                    {
+						canSnakeMove = false;
+						Console.WriteLine(obj.Name);
+					}
+                }
+            }
 		}
 		
 		void MainFormKeyDown(object sender, KeyEventArgs e)
